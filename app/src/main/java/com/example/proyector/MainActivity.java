260a165308ba65e45a2cb0f7173766daf64afedc
EGPerezR <19170161@uttcampus.edu.mx>
2020-11-30 21:36:37 -0600

    "Permisos"

--- app/src/main/java/com/example/proyector/MainActivity.java
+++ app/src/main/java/com/example/proyector/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.proyector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.LOCATION_HARDWARE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.LOCATION_HARDWARE}, Integer.parseInt(LOCATION_SERVICE));
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, Integer.parseInt(LOCATION_SERVICE));
+        }
     }
+
+
 }
