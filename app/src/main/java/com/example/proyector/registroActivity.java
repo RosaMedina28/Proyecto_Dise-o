d8887e17a392dc10a58a7680ad6a714c95ad411f
caricatura25 <19170101@uttcampus.edu.mx>
2020-12-01 16:17:31 -0600

    prueba

--- app/src/main/java/com/example/proyector/registroActivity.java
+++ app/src/main/java/com/example/proyector/registroActivity.java
@@ -2,9 +2,16 @@ package com.example.proyector;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registroActivity extends Activity {
 
     public void evento_registro(View view) {
+    }
+
+    public void evento_register(View view) {
+        EditText campo_apellido = findViewById(R.id.apellido);
+        Toast.makeText(this, campo_apellido.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
