1cce0dc7cac2fc4c1a3ccd4fa8611a631817c864
caricatura25 <19170101@uttcampus.edu.mx>
2020-12-01 14:36:26 -0600

    registro completo

--- app/src/main/java/com/example/proyector/registro.java
+++ app/src/main/java/com/example/proyector/registro.java
@@ -3,12 +3,14 @@ package com.example.proyector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class registro extends AppCompatActivity {
-
+    EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
     }
 }
